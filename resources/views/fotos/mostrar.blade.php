773275b9593b228a45c56bbd67784b1026bfb5d0
deivisjl <deivisjl@gmail.com>
2016-12-19 08:59:21 -0600

    Refactorizando codigo
    Algunas funcionalidades han dado problemas

--- resources/views/fotos/mostrar.blade.php
+++ resources/views/fotos/mostrar.blade.php
@@ -11,7 +11,6 @@
 @endif
 
 <div class="container-fluid">
-<p><a href="/validado/fotos/crear-foto?id={{$id}}" class="btn btn-primary" role="button">Agregar fotos</a></p>
 @if(sizeof($fotos) > 0)
 	@foreach($fotos as $foto)
 		<div class="row">
